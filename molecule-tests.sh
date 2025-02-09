ff339d5ed7fe7ece1f72ba860d0f630c4df8798f
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-02-09 16:44:43 -0500

    Changed testing script order.

--- molecule-tests.sh
+++ molecule-tests.sh
@@ -8,7 +8,7 @@ molecule test -s mssql_db_user
 molecule test -s mssql_db_permission
 molecule test -s mssql_db_object_permission
 
+MOLECULE_BOX="rocky9_cis" molecule test -s mssql_login_windows
 MOLECULE_BOX="rocky8_cis" molecule test -s mssql_login_windows
 MOLECULE_BOX="ubuntu2204_base" molecule test -s mssql_login_windows
-MOLECULE_BOX="rocky9_base" molecule test -s mssql_login_windows
 MOLECULE_BOX="ubuntu2004_base" molecule test -s mssql_login_windows
