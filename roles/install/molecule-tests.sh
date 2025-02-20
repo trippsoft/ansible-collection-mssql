b2f7647ac15ccfdabd6662d6258aeae1112857d4
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-02-20 13:36:53 -0500

    Changed Rocky 9 tests to include CIS hardening and order of tests.

--- roles/install/molecule-tests.sh
+++ roles/install/molecule-tests.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-MOLECULE_BOX="rocky8_cis" molecule test -s linux
+MOLECULE_BOX="rocky9_cis" molecule test -s linux
 MOLECULE_BOX="w2022_cis" molecule test -s win
 MOLECULE_BOX="ubuntu2204_base" molecule test -s linux
 
-MOLECULE_BOX="rocky9_base" molecule test -s linux
+MOLECULE_BOX="rocky8_cis" molecule test -s linux
 
 MOLECULE_BOX="w2019_cis" molecule test -s win
 MOLECULE_BOX="w2025_base" molecule test -s win
